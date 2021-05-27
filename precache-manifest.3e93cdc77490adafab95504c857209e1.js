520961eab23f0e8be6ea85252f39e4114de35f09
punit <kutepunit750@gmail.com>
2021-05-27 12:00:28 +0530

    Updates

---
+++ precache-manifest.3e93cdc77490adafab95504c857209e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d07427c87e1438ea7a2d2687804a980",
+    "url": "/react-codepen-clone/index.html"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/react-codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "88731aea7cb1398e7332",
+    "url": "/react-codepen-clone/static/css/main.d6132aad.chunk.css"
+  },
+  {
+    "revision": "70c3ff1a8dd6beff5ca9",
+    "url": "/react-codepen-clone/static/js/2.4254f81d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codepen-clone/static/js/2.4254f81d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88731aea7cb1398e7332",
+    "url": "/react-codepen-clone/static/js/main.77050e6b.chunk.js"
+  },
+  {
+    "revision": "583799b05e88ee4ecca8",
+    "url": "/react-codepen-clone/static/js/runtime-main.1fe374de.js"
+  }
+]);
